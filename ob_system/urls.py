421e9cf623291f4cf7502c4a7def0460a07adf87
peter soleeh <petersoleeh@gmail.com>
2018-01-12 11:48:11 +0300

    occurrence book urls

--- ob_system/urls.py
+++ ob_system/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     #the landing page
-    url(r'^$',views.index,name='index')
+    url(r'^$',views.index,name='index'),
+
+    # occurrence book page
+    url(r'^ob/',views.occurrence_book,name='occurrence_book')
 ]
--- ob_system/views.py
+++ ob_system/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def occurrence_book(request):
+    return render(request,'occurrence-book/occurrence.html')
