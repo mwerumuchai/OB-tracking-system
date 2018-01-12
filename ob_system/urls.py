d8cdb6b8f979d6547780740322735b84d98379f1
peter soleeh <petersoleeh@gmail.com>
2018-01-12 12:11:52 +0300

    cash bail book urls

--- ob_system/urls.py
+++ ob_system/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    #the landing page
+    #the landing url
     url(r'^$',views.index,name='index'),
 
-    # occurrence book page
-    url(r'^ob/',views.occurrence_book,name='occurrence_book')
+    # occurrence book url
+    url(r'^occurrence_book/',views.occurrence_book,name='occurrence_book'),
+
+    # archives url
+    url(r'^archives/',views.archives,name='archives'),
+
+    # Cash bail book url
+    url(r'^cash_bail_book',views.cash_bail,name='cash_bail')
 ]
