449c7eafd8b5f36542f86ff0460371503d8d2542
mwerumuchai <mwerumuchai@gmail.com>
2018-01-13 12:56:21 +0300

    add info in admin profile

--- ob_system/admin.py
+++ ob_system/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,CriminalProfile,OccurrenceBook,Crime,Report,Booking
 
 admin.site.register(Profile)
+admin.site.register(CriminalProfile)
+admin.site.register(OccurrenceBook)
+admin.site.register(Crime)
+admin.site.register(Report)
+admin.site.register(Booking)
