0e0936544e687d1b48ba6b55c91471c52ca2e6b9
peter soleeh <petersoleeh@gmail.com>
2018-01-16 10:58:58 +0300

    change db

--- occurrence_book/settings.py
+++ occurrence_book/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'karao',
-        'USER': 'erick',
-        'PASSWORD': 'Ngea!2017##',
+        'USER': 'soleeh',
+        'PASSWORD': 'soleeh..',
     }
 }
 
