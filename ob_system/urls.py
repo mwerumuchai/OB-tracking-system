95007c4e91854684ba74733088dd7756bc85c253
Erick <rick.mutua12@gmail.com>
2018-01-23 17:31:36 +0300

    minor updates and modifications

--- ob_system/urls.py
+++ ob_system/urls.py
@@ -24,8 +24,6 @@ urlpatterns = [
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
             core_views.activate, name='activate'),
 
-    path('occurrence_book/report/', views.report, name='report'),
-
     path('occurrence_book/create-criminal-profile/', views.create_criminal_profile,
          name='create-criminal-profile'),
 
