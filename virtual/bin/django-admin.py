35ee00477be0799ce7323e65fc80cb7fca9af151
Erick <rick.mutua12@gmail.com>
2018-01-31 12:56:37 +0300

    added virtual file

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/erick/Documents/personal projects/OB-tracking-system/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
